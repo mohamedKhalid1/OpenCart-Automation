6392043a8a8ea423ef7666057fce24c184669516
mohamedbenkhalid <mohamedbenkhalid2064@gmail.com>
2026-02-28 01:27:35 +0200

    Add tests to TestNG

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -17,8 +17,8 @@ public class BaseTest {
         driver.get("http://tutorialsninja.com/demo/");
     }
 
+    @AfterMethod
     public void tearDown() {
         driver.quit();
     }
-    //http://tutorialsninja.com/demo/
 }
